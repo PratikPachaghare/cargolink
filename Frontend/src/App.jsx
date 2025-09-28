4b32ba20ef1a6c0b89c327076aa466a243f5d4a3
fascavePratik <pratik.p@fascave.com>
2025-09-28 14:53:35 +0530

    install tailwind and create sub folders

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,17 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import logo from './assets/Gemini_Generated_Image_docx7sdocx7sdocx.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="text-3xl font-bold underline">
       hello Cargo! push test
-    </>
+
+      <img src={logo} alt="" className='w-1/2 m-auto mt-28' />
+    </div>
   )
 }
 
